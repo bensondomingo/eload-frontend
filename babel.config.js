e621ff19aac6038c799aba1642ed82a26c17fe7c
Benson Domingo <benson.domingo@outlook.com>
2020-10-02 08:48:15 +0800

    disable logs

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 const removeConsolePlugin = [];
-if (process.env.NODE_ENV === 'production') {
+if (process.env.VUE_APP_DISABLE_CONSOLE_LOG === 'YES') {
   removeConsolePlugin.push('transform-remove-console');
 }
 
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
-  // plugins: removeConsolePlugin
+  plugins: removeConsolePlugin
 };
